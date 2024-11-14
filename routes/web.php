0b75cfbe167af4b25525b4b50e2fad6ab45c3f78
Misaeltp <tmisaelpereztre1555@gmail.com>
2024-11-14 00:31:57 -0600

    Se agrego el dashmenu con los layouts del sidebar, header y estructura

--- routes/web.php
+++ routes/web.php
@@ -112,3 +112,8 @@ return view('admin.layouts.dash');
 });
 
 Route::get('/formulario/agregar/Producto', [formulariosController::class, 'formularioProducto'])->name('agregar.producto');
+
+Route::get('/dashmenu', function () {
+    return view('admin.dashMenu');
+});
+
