2ae30c28501829b8802466934018e26e3a2f5094
reyes200205 <jorgerenteriareyes4@gmail.com>
2024-11-29 10:52:11 +0000

    se agregaron modelos por error

---
+++ app/Models/Direccion.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Direccion extends Model
+{
+    protected $table = 'direcciones';
+
+    protected $fillable = [
+    'calle',
+    'colonia',
+    'numero_ext',
+    'numero_int',
+    'estado',
+    'codigo_postal',
+    'pais',
+    ];
+     //
+    public function envios()
+    {
+    return $this->hasMany(envios::class, 'direcciones_id');
+    }
+    public function persona()
+    {
+        return $this->hasOne(Persona::class, 'direcciones_id');
+    }
+
+}
---
+++ app/Models/Persona.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    Use HasFactory;
+    protected $table = 'personas';
+
+    protected $fillable = [
+        'users_id',
+        'direcciones_id',
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+        'genero',
+        'numero_telefonico'
+        ];
+
+        public function user()
+        {
+            return $this->belongsTo(User::class, 'users_id');
+        }
+    
+        // Relación 1 a 1 con el modelo Direccion
+        public function direccion()
+        {
+            return $this->belongsTo(direccion::class, 'direcciones_id');
+        }
+    
+        // Relación 1 a muchos con el modelo TipoPersona
+        public function tipoPersona()
+        {
+            return $this->hasMany(tipoPersona::class, 'personas_id');
+        }
+}
