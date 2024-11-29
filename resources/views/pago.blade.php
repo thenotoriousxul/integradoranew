37324adf5a70fb718487908f1f7ca42ec9fe0e89
thenotoriousxul <saulsanchezlopez999@gmail.com>
2024-11-28 21:25:59 -0600

    kwai

--- resources/views/pago.blade.php
+++ resources/views/pago.blade.php
@@ -14,7 +14,7 @@
             padding: 10px;
             border: 1px solid #ccc;
             border-radius: 5px;
-            background: #f8f9fa;
+            background: #ffffff;
             margin-bottom: 1rem;
         }
         #submit-payment {
