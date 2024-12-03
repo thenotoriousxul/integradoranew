e20d35bc49f4df1d21b076dd22da509948ef1455
ZeckteK <24170023@uttcampus.edu.mx>
2024-12-03 00:51:24 -0600

    Salsa Verde

--- resources/views/admin/ordenes/reporteVentas.blade.php
+++ resources/views/admin/ordenes/reporteVentas.blade.php
@@ -4,7 +4,7 @@
 
 @section ('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css">
 @endsection
 
 @section('content')
@@ -64,11 +64,10 @@
 
 @section('js')
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
 <script>
-    $(document).ready(function() {
-        $('#ReporteVentas').DataTable(); // Inicializa DataTable
-    });
+   new DataTable('#ReporteVentas');
 </script>
 @endsection
